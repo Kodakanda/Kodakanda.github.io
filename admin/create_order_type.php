3e7720fb79007faca93e73d9186238ba3c464f41
Kodakanda <dagaz888@inbox.ru>
2020-05-15 22:19:20 +0300

    создание типов заказов

---
+++ admin/create_order_type.php
@@ -0,0 +1,35 @@
+<?php
+  if(isset($_POST['order_type']) && isset($_POST['approx_price'])){
+    /////будет убрано
+    require('../db.php');
+    $dbInterface = new DBInterface();
+    /////////
+    $orderType = $_POST['order_type'];
+    $approxPrice = $_POST['approx_price'];
+    $sql = "INSERT INTO order_types (order_type, approx_price) VALUES ('$orderType', $approxPrice)";
+    $result = $dbInterface->db->query($sql);
+    if($result)
+      echo 'SUCCESS';
+    else
+      echo 'FAILED';
+  }
+?>
+<html>
+  <head>
+  </head>
+  <body>
+    <div>
+      <form method="POST">
+        Order Type
+        <input type="text" name="order_type"/>
+        Approx price
+        <input type="text" name="approx_price"/>
+        <input type="submit"/>
+      </form>
+    </div>
+  </body>
+
+
+
+
+</html>
