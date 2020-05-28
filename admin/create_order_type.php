8746ab988400a64d1fef4d72d5b040532eb148b5
Kodakanda <dagaz888@inbox.ru>
2020-05-28 13:15:40 +0300

    валидация форм

--- admin/create_order_type.php
+++ admin/create_order_type.php
@@ -1,34 +1,62 @@
 <?php
-  if(isset($_POST['order_type']) && isset($_POST['approx_price'])){
-    $orderType = $_POST['order_type'];
-    $approxPrice = $_POST['approx_price'];
-    $sql = "INSERT INTO order_types (order_type, approx_price) VALUES ('$orderType', $approxPrice)";
-    $result = $dbInterface->db->query($sql);
-    if($result)
-      echo 'SUCCESS';
-    else
-      echo 'FAILED';
-  }
+    if (isset($_POST['order_type']) && isset($_POST['approx_price'])) {
+	    $order_type_name = trim($_POST['order_type']);
+	    $order_type_approx_price = $_POST['approx_price'];
+	    $errors = [];
+	    $success = false;
+	    if (empty($order_type_name) || mb_strlen($order_type_name) < 2 || mb_strlen($order_type_name) > 25) {
+	        $errors['order_type_name'] = 'Название типа заказа должно быть от 2 до 25 символов!';
+        }
+	    if (!is_numeric($order_type_approx_price) || $order_type_approx_price <= 0) {
+		    $errors['order_type_approx_price'] = 'Стоимость должна быть больше 0!';
+	    }
+	    if (empty($errors)) {
+		    $sql = "INSERT INTO order_types (order_type, approx_price) VALUES (:order_type_name, :order_type_approx_price)";
+		    $sth = $dbInterface->db->prepare($sql);
+		    $sth->execute([
+			    'order_type_name' => $order_type_name,
+			    'order_type_approx_price' => $order_type_approx_price
+		    ]);
+		    $success = true;
+		    $order_type_name = "";
+		    $order_type_approx_price = 0;
+        }
+    }
 ?>
 <html>
-<div>
-  <form method="POST">
-    <label for="type">Название типа заказа</label>
-    <input id="type" type="text" name="order_type"/>
-    <label for="price">Приблизительная стоимость категории</label>
-    <input type="text" id="price" name="approx_price"/>
-    <input type="submit" value="Создать тип заказа"/>
-  </form>
-</div>
+    <div>
+        <form method="POST">
+            <?php if (!empty($errors)):?> <span class="form_error">Проверьте форму на ошибки!</span> <?php endif;?>
+            <?php if ($success):?> <span class="form_success">Новый тип заказа успешно добавлен!</span> <?php endif;?>
+            <label for="type">Название типа заказа <span class="form_required">*</span></label>
+	        <?php if (isset($errors['order_type_name'])):?><span class="error"><?=$errors['order_type_name'];?></span><?php endif;?>
+            <input id="type" type="text" required value="<?= $order_type_name;?>" name="order_type"/>
+            <label for="price">Приблизительная стоимость категории <span class="form_required">*</span></label>
+	        <?php if (isset($errors['order_type_approx_price'])):?><span class="error"><?=$errors['order_type_approx_price'];?></span><?php endif;?>
+            <input type="text" id="price" value="<?= $order_type_approx_price;?>" required name="approx_price"/>
+            <input type="submit" value="Создать тип заказа"/>
+        </form>
+    </div>
 </html>
 <style>
-	form{
-		display: flex;
-    flex-direction: column;
-    width: 50%;
-    margin: auto;
-	}
-	form input{
-		margin-top: 5px;
-	}
+    form {
+        display: flex;
+        flex-direction: column;
+        width: 50%;
+        margin: auto;
+    }
+    form input {
+        margin-top: 5px;
+    }
+    .form_required, .form_error {
+        color: red;
+        font-weight: bold;
+    }
+    .form_success {
+        color: green;
+        font-weight: bold;
+    }
+    .error {
+        color: red;
+    }
 </style>
--- items/form.php
+++ items/form.php
@@ -1,42 +1,81 @@
 <?php
-		$order_types_sql = $dbInterface->db->query("SELECT * FROM order_types");
-		$order_types = $order_types_sql->fetchAll();
-
-	if(isset($_POST['client_name']) && isset($_POST['order_type_id']) && isset($_POST['client_phone']) && isset($_POST['order_comment'])){
-		$clientName = $_POST['client_name'];
-		$orderTypeId = $_POST['order_type_id'];
-		$clientPhone = $_POST['client_phone'];
-		$orderComment = $_POST['order_comment'];
-		$sql = "INSERT INTO 
-				orders (phone_number, client_name, comment, order_type_id) 
-			VALUES 
-				('$clientPhone', '$clientName', '$orderComment', $orderTypeId)";
-		$dbInterface->db->query($sql);
-	}
+    $order_types_sql = $dbInterface->db->query("SELECT * FROM order_types");
+    $order_types = $order_types_sql->fetchAll();
+    if (isset($_POST['client_name']) && isset($_POST['order_type_id']) && isset($_POST['client_phone']) && isset($_POST['order_comment'])){
+        $errors = [];
+        $success = false;
+        $client_name = trim($_POST['client_name']);
+        $client_phone = trim($_POST['client_phone']);
+        $order_comment = trim($_POST['order_comment']);
+        $order_type_id = intval($_POST['order_type_id']);
+        if (!empty($client_name) && (mb_strlen($client_name) < 2 || mb_strlen($client_name) > 35 || !preg_match('/^[а-яА-Я\sa-zA-Z]+$/u', $client_name))) {
+            $errors['client_name'] = 'Укажите настоящее имя!';
+        }
+        if (empty($client_phone) || !preg_match('/^\+7[0-9]{10}$/', $client_phone)) {
+            $errors['client_phone'] = 'Укажите корректный номер телефона!';
+        }
+        if (!empty($order_comment) && (mb_strlen($order_comment) < 2 || mb_strlen($order_comment) > 150)) {
+            $errors['order_comment'] = 'Комментарий должен быть от 2 до 150 символов!';
+        }
+        if (empty($errors)) {
+            $sql = "
+                INSERT INTO orders (phone_number, client_name, comment, order_type_id) 
+                VALUES (:client_phone, :client_name, :order_comment, :order_type_id)
+            ";
+            $sth = $dbInterface->db->prepare($sql);
+            $sth->execute([
+                'client_phone' => $client_phone,
+                'client_name' => $client_name,
+                'order_comment' => $order_comment,
+                'order_type_id' => $order_type_id
+            ]);
+            $success = true;
+            $client_name = "";
+            $client_phone = "";
+            $order_comment = "";
+            $order_type_id = 0;
+        }
+    }
 ?>
 <p><b>Оформите заказ онлайн!</b></p>
 <form class="" method="POST">
-	<label for="client-name">Имя</label>
-	<input id="client-name" type="text" name="client_name">
-	<label for="order-type">Тип заказа</label>
-	<select id="order-type" name="order_type_id">
-		<?php foreach($order_types as $order_type) :?>
-			<option value="<?= $order_type['id'];?>"><?= $order_type['order_type'];?></option>
-		<?php endforeach;?>
-	</select>
-	<label for="client-phone">Номер телефона</label>
-  <input id="client-phone" type="phone" name="client_phone">
-	<label for="order-comment">Комментарий</label>
-  <input id="order-comment" type="textarea" name="order_comment">
-  <input type="submit" value="Отправить">
+    <?php if (!empty($errors)):?> <span class="form_error">Проверьте форму на ошибки!</span> <?php endif;?>
+    <?php if ($success):?> <span class="form_success">Ваша заявка принята!</span> <?php endif;?>
+    <label for="client-name">Имя</label>
+    <?php if (isset($errors['client_name'])):?><span class="error"><?=$errors['client_name'];?></span><?php endif;?>
+    <input id="client-name" type="text" name="client_name" value="<?= $client_name; ?>">
+    <label for="order-type">Тип заказа</label>
+    <select id="order-type" name="order_type_id">
+    <?php foreach($order_types as $order_type) :?>
+        <option <?php if ($order_type_id == $order_type['id']):?> selected <?php endif;?>value="<?= $order_type['id'];?>"><?= $order_type['order_type'];?></option>
+    <?php endforeach;?>
+    </select>
+    <label for="client-phone">Номер телефона <span class="form_required">*</span></label>
+    <?php if (isset($errors['client_phone'])):?><span class="error"><?=$errors['client_phone'];?></span><?php endif;?>
+    <input id="client-phone" type="phone" name="client_phone" required value="<?= $client_phone; ?>">
+    <label for="order-comment">Комментарий</label>
+    <?php if (isset($errors['order_comment'])):?><span class="error"><?=$errors['order_comment'];?></span><?php endif;?>
+    <input id="order-comment" type="textarea" name="order_comment" value="<?= $order_comment; ?>">
+    <input type="submit" value="Отправить">
 </form>
 
 <style>
-	form{
-		display: flex;
-		flex-direction: column;
-	}
-	form input{
-		margin-top: 5px;
-	}
+    form {
+        display: flex;
+        flex-direction: column;
+    }
+    form input {
+        margin-top: 5px;
+    }
+    .form_required, .form_error {
+        color: red;
+        font-weight: bold;
+    }
+    .form_success {
+        color: green;
+        font-weight: bold;
+    }
+    .error {
+        color: red;
+    }
 </style>
